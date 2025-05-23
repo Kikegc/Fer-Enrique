c113aaf6b4d72bb9a509b7b0e3ddabef18195e77
Yuli 777 <3nriquescorcia@gmail.com>
2025-05-23 08:07:21 -0600

    actualizacion

--- src/main/java/mx/poo/itao/Main.java
+++ src/main/java/mx/poo/itao/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    	//HOLA FERXA
     	 Documentos documento = new Documentos();
 	     documento.llamarDocumentos ();
         Scanner sc = new Scanner(System.in);
